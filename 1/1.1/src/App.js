1e46b60a7f36cb93d7431b40102785634f682d69
Jussi <jussi.kukko@gmail.com>
2022-06-11 12:14:54 +0300

    1.1 Header toimii

--- 1/1.1/src/App.js
+++ 1/1.1/src/App.js
@@ -1,50 +1,41 @@
 const Header = (props) => {
-  console.log(props)
-  return <h1>{props.course}</h1>
-}
-
-const Content = (props) => {
   return (
     <div>
-        <part1></part1>
-        <part2></part2>
-        <part3></part3>
+      <h1>{props.course}</h1>
     </div>
   )
 }
 
-const Total = (props) => {
-  return (
-    <div>
-      <p>
-        Total:
-      </p>
-    </div>
-  )
+const Content = () => {
+  
+}
+
+const Total = () => {
+
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const parts = [
-  {
-    name: 'Fundamentals of React',
-    exercises: 10
-  },
-  { 
-  name: 'Using props to pass data',
-  exercises: 7
-  },
-  {
-  name: 'State of a component',
-  exercises: 14
-  }
-]
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
 
   return (
     <div>
       <Header course = {course} />
-      <Content content = {content} />
-      <Total total = {total} />
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   )
 }
