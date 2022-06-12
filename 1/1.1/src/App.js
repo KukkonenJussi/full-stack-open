7e210cee7c95af30c853d4272a3b9800e91ffc26
Jussi <jussi.kukko@gmail.com>
2022-06-12 12:23:52 +0300

    1.1 valmis oikeasti

--- 1/1.1/src/App.js
+++ 1/1.1/src/App.js
@@ -6,38 +6,23 @@ const Header = (props) => {
   )
 }
 
-const Total = () => {
-  return(
+const Total = (props) => {
+  return (
     <div>
       <p>
-
+        Total of exercises {props.ex}
       </p>
     </div>
   )
-}
-
-const Part = (props) => {
-  return(
-    <div>
-      <p>
-        {props.parts}
-      </p>
-    </div>
-  )
-
 }
 
 const Content = (props) => {
-  return(
+  return (
     <div>
-      <p>
-        
-      </p>
+      <p>{props.part} {props.exercises}</p>
     </div>
   )
-
 }
-
 
 const App = () => {
   const course = 'Half Stack application development'
@@ -51,10 +36,10 @@ const App = () => {
   return (
     <div>
       <Header course = {course} />
-      <Part parts = {part1 + exercises1} />
-      <Part parts = {part2 + exercises2} />
-      <Part parts = {part3 + exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Content part = {part1} exercises = {exercises1} />
+      <Content part = {part2} exercises = {exercises2} />
+      <Content part = {part3} exercises = {exercises3} />
+      <Total ex = {exercises1 + exercises2 + exercises3}/>
     </div>
   )
 }
