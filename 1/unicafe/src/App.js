12422bf5c8f0eb6d0045875322f6222d559c658c
Jussi <jussi.kukko@gmail.com>
2022-06-21 12:11:17 +0300

    1.10 Buttonit DONE

--- 1/unicafe/src/App.js
+++ 1/unicafe/src/App.js
@@ -23,8 +23,15 @@ const Statistics = ({good, neutral, bad}) => {
   }
 }
 
+// Määritellään nappi. Napille annetaan toiminto ja nimi.
+const Button = (props) => (
+  <button onClick = {props.handleClick}>
+    {props.text}
+  </button>
+)
+
 const App = () => {
-  // tallenna napit omaan tilaansa
+
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -32,18 +39,10 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      
-      <button onClick = {() => setGood(good + 1)}>
-        Good 
-      </button>
-      
-      <button onClick = {() => setNeutral(neutral + 1)}>
-        Neutral
-      </button>
-      
-      <button onClick = {() => setBad(bad + 1)}>
-        Bad
-      </button> 
+
+      <Button handleClick = {() => setGood(good + 1)} text = "Good" />
+      <Button handleClick = {() => setNeutral(neutral + 1)} text = "Neutral" />
+      <Button handleClick = {() => setBad(bad + 1)} text = "Bad" />
       
       <h1>Statistics</h1>
       
