a44a3b139ad1856a216379204527a74522fc0904
Jussi <jussi.kukko@gmail.com>
2022-07-05 11:30:46 +0300

    2.3 DONE

--- 2/kurssitiedot/components/Course.js
+++ 2/kurssitiedot/components/Course.js
@@ -14,53 +14,52 @@ const Course = (props) => {
 
 // Määritellään kurssin otsikko.
 const Header = (props) => {
-  const {course} = props
+const {course} = props
 
-  return (
-      <h1>{course.name}</h1>
-  )
+return (
+    <h1>{course.name}</h1>
+)
 }
 
 // Määritellään Content -osio. Hakasuluissa oleva numero viittaa listassa olevaan indeksin arvoon.
 const Content = (props) => {
-  const {course} = props
+const {course} = props
 
-  return (
-    <div>
-      {course.parts.map(component =>
-        <Part key = {component.id}
-        part = {component.name} 
-        exercises = {component.exercises} />
-    )}
-    </div>
-  )
+return (
+  <div>
+    {course.parts.map(component =>
+      <Part key = {component.id}
+      part = {component.name} 
+      exercises = {component.exercises} />
+  )}
+  </div>
+)
 }
 
 const Total = (props) => {
-  const {course} = props
-  
-  let total = 0 // Alustetaan muuttuja, jota käytetään tehtävien yhteenlaskemista varten
-  
-  course.parts.forEach(ex => { // Käydään foreach -loopissa läpi kaikki kurssin harjoitukset ja lasketaan ne yhteen
-    total += ex.exercises
-  });
+const {course} = props
 
+/* Tehtävien yhteenlaskettu lukumäärä reducen avulla. Total muuttuja
+saaarvon nolla, johon sitten lisätään loopissa kaikki ex muuttujat*/
+let total = course.parts.reduce((total, ex) => {
+  return total + ex.exercises
+}, 0) 
 
-  // Palautetaan tehtävien yhteenlaskettu määrä
-  return (
-    <div>
-      Number of exercises {total}
-    </div>
-  )
+// Palautetaan tehtävien yhteenlaskettu määrä
+return (
+  <div>
+    Number of exercises {total}
+  </div>
+)
 }
 
 // Määritellään Part -osio.
 const Part = (props) => {
-  return (
-    <p>
-      {props.part} {props.exercises} 
-    </p>
-  )
+return (
+  <p>
+    {props.part} {props.exercises} 
+  </p>
+)
 }
 
 export default Course
