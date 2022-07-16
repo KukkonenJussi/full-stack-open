9911a23f34c656288de3a1df8d8a991997116f4b
Jussi <jussi.kukko@gmail.com>
2022-07-16 11:58:58 +0300

    2.7 DONE

--- 2/puhelinluettelo/App.js
+++ 2/puhelinluettelo/App.js
@@ -3,10 +3,15 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([ // Lista henkilöistä
-    { name: 'Arto Hellas', id: 1}
+    { name: 'Arto Hellas', id: 1, number: '040-123456'},
+    { name: 'Ada Lovelace', id: 2, number: '39-44-5323523'},
+    { name: 'Dan Abramov', id: 3, number: '12-43-234345'},
+    { name: 'Mary Poppendieck', id: 4, number: '39-23-6423122'},
   ]) 
+  
   const [newName, setNewName] = useState('') // Nimen lisäystä varten
   const [newNumber, setNewNumber] = useState('') // Numeron lisäystä varten
+  // const [filterName, setFilterName] = useState('') // Filtteröintiä varten
 
   /*Osio, jonka avulla lisätään henkilöitä puhelinluetteloon*/
   const addPerson = (event) => {
@@ -17,7 +22,11 @@ const App = () => {
       id: persons.length + 1,
       number: newNumber,
     }
-    
+
+    // Henkilöä lisätessä tarkistetaan, löytyykö uusi nimi jo olemassa olevasta listasta
+    persons.find(person => person.name === newName)
+      ? alert(`${newName} is already added to phonebook`) // Annetaan varoitus (ja ei anneta lisätä nimeä uudelleen), mikäli nimi on jo listassa
+      : // Muussa tapauksessa lisätään uusi nimi listalle
     // Lisää henkilön listalle. Henkilölle tulee nimi ja id
     setPersons(persons.concat(personObject)) // uusi henkilö lisätään luetteloon
     setNewName('') // Kenttä tyhjenee add -napin painamisen jälkeen
@@ -40,6 +49,10 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        Filter shown with: <input />
+      </div>
+      <h3>Add a new</h3>
       <form onSubmit = {addPerson}>
         <div>
           name: <input value = {newName}
@@ -53,7 +66,7 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
-      <h2>Numbers</h2>
+      <h3>Numbers</h3>
       <div>
         {persons.map(person => 
           <Person key = {person.id} person = {person} /> 
