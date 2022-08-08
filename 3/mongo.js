d471ce20a07ec7236ae27889e1ac861123a28448
Jussi <jussi.kukko@gmail.com>
2022-08-08 14:35:34 +0300

    3.12 - 99% done

--- 3/mongo.js
+++ 3/mongo.js
@@ -12,36 +12,52 @@ const url = `mongodb+srv://Jussi:${password}@cluster0.f6hib1k.mongodb.net/phoneb
 
 mongoose.connect(url) // Avataan yhteys
 
-// Yhteyden avaamisen jälkeen määritellään skeema.
+/* Yhteyden avaamisen jälkeen määritellään skeema. 
+Määritetään nimi neljänneksi ja numero viidenneksi argumentiksi, 
+koska salasana on kolmas argumentti.*/
 const personSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    number: String,
+    name: String, // Nimi on kolmas indeksi eli neljäs argumentti
+    number: String, // Numero on neljäs indeksi eli viides argumentti
 })
 
 // Määritellään skeemaa vastaava model.
 const Person = mongoose.model('Person', personSchema)
 
-// Tehdään esimerkki, joka lisätään tietokantaan
-const person = new Person ({
-    id: 5,
-    name: "Rytkön Ville",
-    number: "0400-111222"
+/* Määritetään oliomuuttuja person uudelleen, jotta argumenttien tiedot
+voidaan laittaa nimen ja numeron muuttujien kohdalle.*/
+const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
 })
 
-/* Tallettaminen tapahtuu metodilla save. Metodi palauttaa promisen,
-jolle voidaan rekisteröidä then -metodin avulla tapahtumakäsittelijä.
+/* Haetaan tallennetut henkilöt tietokannasta,
+tulostetaan heidän nimi ja puhelinnumero konsoliin*/
+if (process.argv.length === 3) {
+    Person
+        .find({}) // Haetaan oliot kannasta Person metodin find avulla. Koska find -metodin parametri on tyhjä olio {}, kaikki kokoelman Person oliot haetaan.
+        .then(result => {
+        console.log('Phonebook:\n- - - - -')
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        });
+        mongoose.connection.close()
+    })
+}
 
-person.save().then(result => { // Olion tallettamisen jälkeen kutsutaan parametrina olevaa tapahtumakäsittelijää,
-    console.log('person added!')
-    mongoose.connection.close() // joka sulkee yhteyden tietokantaan. Tärkeää, koska muutoin ohjelman suoritus EI pääty.
-})
-*/
+/* Tallennetaan olio ja annetaan ilmoitus, jos käynnistettäessä on annettu kolme komentoparametria. */
+if (process.argv.length === 5) {
+    person
+        .save() // Tallennetaan olio
+        .then(result => { // Olion tallettamisen jälkeen kutsutaan parametrina olevaa tapahtumakäsittelijää,
+        console.log(`added ${person.name} number ${person.number} to phonebook`) // Tulostetaan konsoliin teksti henkilön lisäyksestä.
+        mongoose.connection.close() // Suljetaan yhteys tietokantaan. Tärkeää, koska muutoin ohjelman suoritus EI pääty.
+    })
+}
 
-// Haetaan tallennetut henkilöt tietokannasta ja tulostetaan ne konsoliin
-Person.find({}).then(result => {
-    result.forEach(person => {
-        console.log(person)
-    });
-    mongoose.connection.close()
-})
+/* Mikäli parametreja ei ole oikea määrä
+(joko numero puuttuu tai henkilön koko nimi ei ole hipsuissa),
+tulostetaan konsoliin virheilmoitus.*/
+if (process.argv.length === 4 || process.argv.length > 5) {
+    console.log('If there are spaces in the name, enclose the name in quotation marks.') // Tulostetaan konsoliin teksti.
+    mongoose.connection.close() // Suljetaan yhteys tietokantaan. Tärkeää, koska muutoin ohjelman suoritus EI pääty.
+}
