01956c2162180d05b01032e4beb01e199c559357
Jussi <jussi.kukko@gmail.com>
2022-08-19 15:17:59 +0300

    3.15 aloitettu

--- 3/index.js
+++ 3/index.js
@@ -1,10 +1,14 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const cors = require('cors')
 const express = require('express')
 const app = express()
-app.use(express.static('build'))
 
+app.use(express.static('build'))
 app.use(express.json()) // Expressin tarjoama json-parseri. Tällä pääsee käsiksi pyynnön mukana lähetettyyn dataan käsiksi ja voidaan lisätä bodyyn resursseja.
 app.use(cors())
+ 
+//const Person = mongoose.model('Person', personSchema)
 
 let persons = [
   {
@@ -30,38 +34,68 @@ let persons = [
 ]
 
 app.get('/api/persons', (req, res) => { // Määritellään handleri (eli tapahtumakäsittelijä), joka hoitaa sovelluksen polkuun /api/persons tulevia HTTP GET -pyyntöjä
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
-const PORT = process.env.PORT || 3001 // Määritetään sovellukselle portti. Joko process.env tai portti 3001.
+const PORT = process.env.PORT // Määritetään sovellukselle portti. Portti määritelty process.envin avulla, eikä suoraan koodiin kirjoitettuna
 app.listen(PORT, () => { // Sidotaan muuttujaan app sijoitettu http -palvelin kuuntelemaan porttiin 3001 tulevia HTTP -pyyntöjä
   console.log(`Server running on port ${PORT}`)
 })
 
-app.get('/api/persons/:id', (request, response) => { // Määritellään yksittäisen resurssin haku. Kaksoispiste tarkoittaa, että annetaan polulle (tässä tapauksessa id) parametreja
-  const id = Number(request.params.id) // Ennen parametreja oleva Number tarkoittaa parametrin muuntamista numeroksi
-  const person = persons.find(person => person.id === id) // find -metodilla haetaan taulukosta parametria vastaava tietue ja palautetaan se pyynnön tekijälle
-  
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+app.get('/api/persons/:id', (request, response, next) => { // Määritellään yksittäisen resurssin haku. Kaksoispiste tarkoittaa, että annetaan polulle (tässä tapauksessa id) parametreja
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)
+      } else { // Jos kannasta haettua oliota ei löydy, muuttuja arvo on null
+        response.status(404).end() // Vastataan statuskoodilla 404
+      }
+  }) 
+    .catch(error => { // Jos id ei ole hyväksyttävässä muodossa
+      console.log(error) // Vastataan kyselyyn statuskoodilla 400
+      response.status(400).send({ errro: 'malformatted id' })
+    })
 })
 
-app.delete('/api/persons/:id', (request, response) => { // Määritellään yksittäisen resurssin poistaminen.
+app.delete('/api/persons/:id', (request, response, next) => { // Määritellään yksittäisen resurssin poistaminen.
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+  
+  /*
   const id = Number(request.params.id)
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+  */
 })
 
 app.post('/api/persons', (request, response) => { // Määritellään uuden resurssin lisäys.
-  
+  const body = request.body
+
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  const person = new Person({
+    name: body.content,
+    number: body.content,
+  })
+
+  person.save().then(savedPerson => {
+    response.json(savedPerson)
+  })
+
+  /*
   const maxId = persons.length > 0
     ? Math.max(...persons.map(n => n.id)) // HUOM! Tehtävänannon mukaan pitäisi generoida joku riittävän iso arvoväli?
     : 0
   
+ 
   const person = request.body 
   person.id = maxId + 1
 
@@ -69,6 +103,8 @@ app.post('/api/persons', (request, response) => { // Määritellään uuden resu
 
   console.log(person)
   response.json(person)
+  */
+
 })
 
 app.get('/', (req, res) => { // Määritellään handleri (eli tapahtumakäsittelijä), joka hoitaa sovelluksen polkuun / tulevia HTTP GET -pyyntöjä
