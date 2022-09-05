2c6272440cf874100033fbe643ebf2905a254271
Jussi <jussi.kukko@gmail.com>
2022-09-05 17:50:05 +0300

    4.3 DONE

--- 4/tests/list_helper.js
+++ 4/tests/list_helper.js
@@ -2,6 +2,14 @@ const dummy = (blogs) => {
     return 1 // Tässä vaiheessa (4.3) funktio palauttaa AINA numeron 1
 }
 
+const totalLikes = (blogs) => {
+    const likes = blogs.reduce((sum, current) => { // Parametreina tulleista blogeista tehdään lista. Sum muuttuja on kokonaissumma, current tarkoittaa listassa olevaa alkiota, jota käsitellään
+        return sum + current.likes
+    }, 0) // Nolla tarkoittaa aloitusarvoa summalle
+    return likes // Palautetaan tykkäysten lukumäärä
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
