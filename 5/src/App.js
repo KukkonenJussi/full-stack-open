04f72bfa9ad8da0a2b1e7c489f626e0e46c39884
Jussi <jussi.kukko@gmail.com>
2023-02-28 18:48:36 +0200

    5.5 & 5.6 DONE

--- 5/src/App.js
+++ 5/src/App.js
@@ -1,23 +1,24 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import loginService from './services/login'
 import blogService from './services/blogs'
 import Notification from './components/Notification'
+import LoginForm from './components/LoginForm'
+import Togglable from './components/Togglable'
+import BlogForm from './components/Blogform'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
   const [message, setMessage] = useState(null)
-  const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
-  const [url, setUrl] = useState('')
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+  const [toggle, setToggle] = useState(false)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
-      setBlogs( blogs )
-    )  
+      setBlogs(blogs)
+    )
   }, [])
 
   useEffect(() => {
@@ -30,65 +31,38 @@ const App = () => {
     }
   }, [])
 
-  const handleTitleChange = (event) => {
-    setTitle(event.target.value)
-    console.log(event)
-  }
 
-  const handleAuthorChange = (event) => {
-    setAuthor(event.target.value)
-    console.log(event)
-  }
-
-  const handleUrlChange = (event) => {
-    setUrl(event.target.value)
-    console.log(event)
-  }
-
-
-  const addBlog = async (event) => {
-    event.preventDefault()
-    
-    const blogObject = {
-      title: title,
-      author: author,
-      url: url,
-    }
-
+  const addBlog = (blogObject) => {
     if (
-      blogObject.title === '' || 
-      blogObject.author === '' || 
+      blogObject.title === '' ||
+      blogObject.author === '' ||
       blogObject.url === ''
-      ) {
+    ) {
+      BlogFormRef.current.toggleVisibility()
       setMessage('Title, author and url needed!')
       setTimeout(() => {
         setMessage(null)
-      }, 5000)
-      setTitle('')
-      setAuthor('')
-      setUrl('')
-    } 
+      }, 2000)
+    }
     else {
       blogService
         .create(blogObject)
-          .then(returnedBlog => {
-            setBlogs(blogs.concat(returnedBlog))
-            setMessage(`A new blog ${blogObject.title} by ${blogObject.author} added`)
-            setTimeout(() => {
-              setMessage(null)
-            }, 5000);
-            setTitle('')
-            setAuthor('')
-            setUrl('')
-          })
+        .then(returnedBlog => {
+          setBlogs(blogs.concat(returnedBlog))
+          BlogFormRef.current.toggleVisibility()
+          setMessage(`A new blog ${blogObject.title} by ${blogObject.author} added`)
+          setTimeout(() => {
+            setMessage(null)
+          }, 5000)
+        })
     }
   }
-  
+
 
   const handleLogin = async (event) => {
     event.preventDefault()
     console.log('loggin in with', username, password)
-  
+
     try {
       const user = await loginService.login({
         username, password,
@@ -110,100 +84,54 @@ const App = () => {
         setMessage(null)
       }, 5000)
     }
-  
+
   }
-  
+
   const handleLogout = async () => {
     window.localStorage.removeItem('loggedBlogappUser')
-    // window.localStorage.clear() // Vaihtoehtoinen tapa: local storagen tilan kokonaan nollaava komento.
     setUsername('')
     setPassword('')
   }
 
-  const blogForm = () => (
-    <form onSubmit={addBlog}>
-      <div>
-      Title: <input
-        name='title'
-        value={title}
-        onChange={handleTitleChange}
-        />
-      </div>
-      <div>
-      Author: <input
-        name='author'
-        value={author}
-        onChange={handleAuthorChange}
-        />
-      </div>
-      <div>
-      Url: <input
-        name='url'
-        value={url}
-        onChange={handleUrlChange}
-        />
-      </div>
-
-      <button type="submit">save</button>
-    </form>  
-  )
-
-  const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-          <input
-          type="text"
-          value={username}
-          name="Username"
-          onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-          <input
-          type="password"
-          value={password}
-          name="Password"
-          onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type="submit">login</button>
-    </form>      
-  )
-
-  if (user === null) {
-    return (
-      <div>
-        <h2>Log in to application</h2>
-
-        <Notification message={message} />
-        
-        {!user && loginForm()}
-        
-      </div>
-    )
-  }
+  const BlogFormRef = useRef()
 
   return (
     <div>
-      <h2>Blogs</h2>
 
+      <h1>Blogs app</h1>
       <Notification message={message} />
 
-      {!user && loginForm()}
-      {user && <div>
-        <p>{user.name} logged in <button onClick={handleLogout} >logout</button></p>
-        {blogForm()}
-      </div>
-    } 
+      {!user && // Näkymä, mikäli ei ole kirjauduttu
+        <Togglable buttonLabel="log in">
+          <LoginForm
+            username={username}
+            password={password}
+            handleUsernameChange={({ target }) => setUsername(target.value)}
+            handlePasswordChange={({ target }) => setPassword(target.value)}
+            handleSubmit={handleLogin}
+          />
+        </Togglable>
+      }
+      {user && // Näkymä, mikäli kirjauduttu
+        <div>
+          <p>{user.name} logged in <button onClick={handleLogout} >logout</button></p>
+          <Togglable buttonLabel="new blog" ref={BlogFormRef}>
+            <BlogForm createBlog={addBlog}/>
+          </Togglable>
 
-      {blogs
-      .map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+          {blogs.map(blog =>
+            <Blog
+              key={blog.id}
+              blog={blog}
+            />
+          )}
+        </div>
+      }
+
     </div>
   )
-}
+
+
+} // App 
 
 export default App
