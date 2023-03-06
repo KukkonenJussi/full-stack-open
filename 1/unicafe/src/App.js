92dc50a93e012c6e483074e7a73ddfdbc407db0c
Jussi <jussi.kukko@gmail.com>
2023-03-06 16:37:13 +0200

    1.11 DONE

--- 1/unicafe/src/App.js
+++ 1/unicafe/src/App.js
@@ -1,41 +1,48 @@
 import { useState } from 'react'
 
 // Määritellään tilastojen näyttämistä varten
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
   // Mikäli palautettta ei ole annettu
   if (good === 0 & neutral === 0 & bad === 0) {
     return (
-    <p>No feedback given</p> // Palautetaan tämä teksti
+      <p>No feedback given</p> // Palautetaan tämä teksti
     )
   }
   // Muussa tapauksessa palautetaan tilastot
   else {
     return (
-      <div>
-        <StatisticsLine text = "Good" value = {good} />
-        <StatisticsLine text = "Neutral" value = {neutral} />
-        <StatisticsLine text = "Bad" value = {bad} />
-        <StatisticsLine text = "All" value = {good + neutral + bad} />
-        <StatisticsLine text = "Average" value = {((good - bad) / (good + neutral + bad)) * 100} />
-        <StatisticsLine text = "Positive" value = {(good / (good + neutral + bad)) * 100 + "%"} />
-      </div>
+      <table>
+        <tbody>
+          <StatisticsLine text="Good:" value={good} />
+          <StatisticsLine text="Neutral:" value={neutral} />
+          <StatisticsLine text="Bad:" value={bad} />
+          <StatisticsLine text="All:" value={good + neutral + bad} />
+          <StatisticsLine text="Average:" value={((good - bad) / (good + neutral + bad)) * 100} />
+          <StatisticsLine text="Positive:" value={(good / (good + neutral + bad)) * 100 + "%"} />
+        </tbody>
+      </table>
     )
   }
 }
 
 // Määritellään StatisticsLine, joka hyödyntää Statisticsia.
-const StatisticsLine = ({text, value}) => {
+const StatisticsLine = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>
+        {text}
+      </td>
+      <td>
+        {value}
+      </td>
+    </tr>
   )
 }
 
 // Määritellään nappi. Napille annetaan toiminto ja nimi.
-const Button = (props) => (
-  <button onClick = {props.handleClick}>
-    {props.text}
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
   </button>
 )
 
@@ -48,11 +55,11 @@ const App = () => {
   return (
     <div>
       <h1>Give feedback</h1>
-      <Button handleClick = {() => setGood(good + 1)} text = "Good" />
-      <Button handleClick = {() => setNeutral(neutral + 1)} text = "Neutral" />
-      <Button handleClick = {() => setBad(bad + 1)} text = "Bad" />
+      <Button handleClick={() => setGood(good + 1)} text="Good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="Neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="Bad" />
       <h1>Statistics</h1>
-      <Statistics good = {good} neutral = {neutral} bad = {bad} />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
 }
