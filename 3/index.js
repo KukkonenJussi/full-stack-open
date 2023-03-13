691c9d7c07b6d23d1b4bc5106d01554cf276817e
Jussi <jussi.kukko@gmail.com>
2023-03-13 15:41:20 +0200

    updated whole part 3: exercises 1-18 DONE

--- 3/index.js
+++ 3/index.js
@@ -3,27 +3,42 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 const Person = require('./models/person')
+
 const morgan = require('morgan')
+morgan.token('body', request => {
+  return JSON.stringify(request.body)
+})
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-app.use(express.json()) // Expressin tarjoama json-parseri. Tällä pääsee käsiksi pyynnön mukana lähetettyyn dataan käsiksi ja voidaan lisätä bodyyn resursseja.
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
 
-app.use(morgan(function (tokens, request, response) {
-  return [
-    tokens.method(request, response),
-    tokens.url(request, response),
-    tokens.status(request, response),
-    tokens.res(request, response, 'content-length'), '-',
-    tokens['response-time'](request, response), 'ms',
-    JSON.stringify(request.body),
-  ].join(' ')
-}))
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
 
 app.use(cors())
-
+app.use(express.json()) // Expressin tarjoama json-parseri. Tällä pääsee käsiksi pyynnön mukana lähetettyyn dataan käsiksi ja voidaan lisätä bodyyn resursseja.
+app.use(requestLogger)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(express.static('build'))
- 
-//const Person = mongoose.model('Person', personSchema)
 
 let persons = [
   {
@@ -52,54 +67,22 @@ app.get('/', (request, response) => { // Määritellään handleri (eli tapahtum
   response.send('<h1>Hello World!</h1>')
 })
 
-/*
-// Joko näin yhdellä rivillä (alapuolella useammalla rivillä. Huomaa '' ja `` ero)
-app.get('/info', (req, res) => {
-  res.send('<div>Phonebook has info for ${persons.length} people <br />${new Date}</div>')
-})
-*/
-
-// Tai näin `` käyttäen
 app.get('/info', (request, response) => {
-  response.send(`
+  Person.find({}).then(persons => {
+    response.send(`
     <div> 
       Phonebook has info for ${persons.length} people <br />
       ${new Date}
     </div>
   `)
-})
-
-app.post('/api/persons', (request, response) => { // Määritellään uuden resurssin lisäys.
-  const body = request.body
-
-  if (body.name === undefined || body.number === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const person = new Person({
-    name: body.name,
-    number: body.number,
-    id: Math.floor(Math.random() * 9000) + 1 
-  })
-
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
   })
 })
-  
 
 app.get('/api/persons', (request, response) => { // Määritellään handleri (eli tapahtumakäsittelijä), joka hoitaa sovelluksen polkuun /api/persons tulevia HTTP GET -pyyntöjä
   Person.find({}).then(persons => {
+    console.log(persons)
     response.json(persons)
   })
-})
-
-app.delete('/api/persons/:id', (request, response, next) => { // Määritellään yksittäisen resurssin poistaminen.
-  Person.findByIdAndRemove(request.params.id)
-    .then(result => {
-      response.status(204).end()
-    })
-    .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => { // Määritellään yksittäisen resurssin haku. Kaksoispiste tarkoittaa, että annetaan polulle (tässä tapauksessa id) parametreja
@@ -110,45 +93,71 @@ app.get('/api/persons/:id', (request, response, next) => { // Määritellään y
       } else { // Jos kannasta haettua oliota ei löydy, muuttuja arvo on null
         response.status(404).end() // Vastataan statuskoodilla 404
       }
-    }) 
+    })
     .catch(error => {
       next(error)
     }) // Virhetilanteen käsittely siirretään eteenpäin funktiolla next
 })
 
-app.put('/api/persons/:id', (request, response, next) => {
+app.post('/api/persons', (request, response, next) => { // Määritellään uuden resurssin lisäys.
   const body = request.body
 
-  const person = {
+  const person = new Person({
+    id: Math.floor(Math.random() * 9000) + 1,
     name: body.name,
     number: body.number,
+  })
+
+  const personExists = persons.find((person) => person.name === body.name)
+
+  if (body.name === undefined) { // Mikäli nimi puuttuu, heitetään errori
+    return response.status(400).json({ error: 'name missing!' })
   }
 
-  Person.findByIdAndUpdate(request.params.id, person, { new: true })
-    .then(updatePerson => {
-      response.json(updatePerson)
+  if (body.number === undefined) { // Mikäli numero puuttuu, heitetään errori
+    return response.status(400).json({
+      error: 'number missing!'
+    })
+  }
+
+  if (personExists) { // Mikäli numero puuttuu, heitetään errori
+    return response.status(400).json({
+      error: 'name must be unique!'
+    })
+  }
+
+
+  person.save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch(error => next(error)) // Validointisäännön rikkovan olion lisätessä (nimi tai numero puuttuu) heittää errorin, eikä ohjelma kaadu
+})
+
+app.delete('/api/persons/:id', (request, response, next) => { // Määritellään yksittäisen resurssin poistaminen.
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
     })
     .catch(error => next(error))
 })
 
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-  
-app.use(unknownEndpoint)
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
 
-const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
-
-app.use(errorHandler)
+app.use(unknownEndpoint) // Tärkeä olla käytössä vasta täällä lopussa, muutoin ei pääse esim. info -sivulle!
+// app.use(errorHandler)
 
 const PORT = process.env.PORT // Määritetään sovellukselle portti. Portti määritelty process.envin avulla, eikä suoraan koodiin kirjoitettuna
 app.listen(PORT, () => { // Sidotaan muuttujaan app sijoitettu http -palvelin kuuntelemaan porttiin tulevia HTTP -pyyntöjä
--- 3/models/person.js
+++ 3/models/person.js
@@ -1,30 +1,31 @@
 const mongoose = require('mongoose')
 
-// MongoDB:n URI -osoite, onka avulla sovelluksemme käyttämä MongoDB-kirjasto saa yhteyden kantaan.
-// `mongodb+srv://Jussi:${password}@cluster0.f6hib1k.mongodb.net/phonebookApp?retryWrites=true&w=majority` tietokannan osoite
-// HUOM! muuta alapuolella oleva url muotoon const url = process.env.MONGODB_URI
-const url = process.env.MONGODB_URI 
+mongoose.set('strictQuery', false)
+
+const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
 mongoose.connect(url) // Avataan yhteys
-    .then(result => { // Viestin onnistuneen yhteyden muodostamisessa
-        console.log('connected to MongoDB') 
-    })
-    .catch((error) => { // Viestin epäonnistuneen yhteyden muodostamisessa
-        console.log('error connecting to MongoDB', error.message)
-    })
+  .then(() => { // Viestin onnistuneen yhteyden muodostamisessa
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => { // Viestin epäonnistuneen yhteyden muodostamisessa
+    console.log('error connecting to MongoDB', error.message)
+  })
 
-/* Yhteyden avaamisen jälkeen määritellään skeema. 
-Määritetään nimi neljänneksi ja numero viidenneksi argumentiksi, 
-koska salasana on kolmas argumentti.*/
 const personSchema = new mongoose.Schema({
-  name: String, // Nimi on kolmas indeksi eli neljäs argumentti
-  number: String, // Numero on neljäs indeksi eli viides argumentti
+  name: { // Nimi on kolmas indeksi eli neljäs argumentti
+    type: String,
+    required: true,
+  },
+  number: { // Numero on neljäs indeksi eli viides argumentti
+    type: String,
+    required: true,
+  },
 })
 
-/* Muotoillaan Mongoosen palauttamat oliot haluttuun muotoon.
-Jätetään pois kentät _id ja __v*/
+// Muotoillaan Mongoosen palauttamat oliot haluttuun muotoon (Jätetään pois kentät _id ja __v)
 personSchema.set('toJSON', {
   transform: (document, returnedObject) => {
     returnedObject.id = returnedObject._id.toString()
@@ -33,7 +34,4 @@ personSchema.set('toJSON', {
   }
 })
 
-/* Moduulin ulos näkyvä osa määritellään arvo muuttujalle module.exports.
-Tässä tapauksessa modelin arvo on Person. Muut moduulin sisällä määritellyt
-asiat, esim. muuttujat mongoose ja url EIVÄT näy moduulin käyttäjälle.*/
 module.exports = mongoose.model('Person', personSchema)
